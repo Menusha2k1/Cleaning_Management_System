bf4d9c248fe614afe4cd746e52343b54a51418b9
Menusha Thathsara <161927072+Menusha2k1@users.noreply.github.com>
2025-04-29 22:19:34 +0530

    Create user authentication backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,24 @@
-const app = require('./app');
-const connectDB = require('./config/db');
-const PORT = process.env.PORT || 5000;
+require("dotenv").config();
+const config = require("./config.json");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-// Connect to database
-connectDB();
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+mongoose.connect(config.connectionString);
+
+const authRoutes = require('./Routes/authRoutes');
+
+app.use("/", authRoutes);
+
+app.listen(8000);
+
+module.exports = app;
+
+
+
+
+
