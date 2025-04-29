d779f8b7f2f930e7a4de55b5136cd37575dd0f91
Menusha Thathsara <161927072+Menusha2k1@users.noreply.github.com>
2025-04-29 21:28:50 +0530

    setup backend

---
+++ backend/Models/serviceModel.js
@@ -0,0 +1,12 @@
+// models/Service.js
+const mongoose = require('mongoose');
+
+const serviceSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Service', serviceSchema);
